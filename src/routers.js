abd36c0fc79edac3ddd560f4a26e62b41430fe0c
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-08-30 10:17:29 +0300

    created the router.js file for routes.

---
+++ src/routers.js
@@ -0,0 +1,23 @@
+import Home from "./components/Home.vue";
+import SignUp from "./components/SignUp.vue";
+
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    name: "Home",
+    component: Home,
+    path: "/",
+  },
+  {
+    name: "SignUp",
+    component: SignUp,
+    path: "/signup",
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;

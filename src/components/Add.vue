fa58cb486b8c12a748392dce50a0a9966cf638af
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-08-30 20:25:59 +0300

    component to add restaurants.

---
+++ src/components/Add.vue
@@ -0,0 +1,41 @@
+<template>
+  <Header/>
+  <h1>Hello, user. This is your add page</h1>
+  <form action="" class="add">
+    <input type="text" placeholder="Enter Name" name="name" v-model="Restaraunt.name">
+    <input type="tel" placeholder="Enter Contact" name="contact" v-model="Restaraunt.contact">
+    <input type="text" placeholder="Enter Address" name="address" v-model="Restaraunt.address">
+    <button type="button" v-on:click="addRes">Add New Res</button>
+  </form>
+</template>
+<script>
+import Header from './Header.vue';
+import axios from "axios";
+export default {
+    name: "Add",
+    components: { Header },
+    data(){
+      return{
+        Restaraunt:{
+          name:'',
+          contact:'',
+          address:'',
+        }
+      }
+    },
+    methods:{
+      async addRes(){
+        let result = await axios.post('http://localhost:3000/restaraunt',{
+          name:this.Restaraunt.name,
+          address:this.Restaraunt.address,
+          contact:this.Restaraunt.contact
+        })
+        console.log(result)
+        if (result.status == 201) {
+        this.$router.push({ name: "Home" });
+      }
+      }
+    }
+};
+</script>
+<style scoped></style>

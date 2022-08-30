cd35632b01ad9ff41e8e397154f14d3927ce9a1e
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-08-30 10:16:34 +0300

    update the app component, import and use signup component.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignUp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SignUp
+}
 }
 </script>
 
